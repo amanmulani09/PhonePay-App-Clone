16a958b2e92062a151ec0758e4ac53411b26ac6f
aman mulani <mulaniaman0504@gmail.com>
2024-01-16 10:51:19 +0530

    build screen components

---
+++ src/screens/Welth.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Welth = () => {
+  return (
+    <View>
+      <Text>Welth</Text>
+    </View>
+  )
+}
+
+export default Welth
+
+const styles = StyleSheet.create({})
